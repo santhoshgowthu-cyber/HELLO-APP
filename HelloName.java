53869ee61b272bccf5d2f2d51ceace56e33dad34
GOUTHU VEERA VENKATA SIVA SANTHOSH <santhoshgowthu@gmail.com>
2026-03-11 13:45:37 +0530

    Fix HelloName logic

--- HelloName.java
+++ HelloName.java
@@ -1,14 +1,14 @@
-public class HelloName {
+public class HelloDefault {
 
     public static void main(String[] args) {
 
+        String name = "World";
+
         if(args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } 
-        else {
-            System.out.println("Hello User");
+            name = args[0];
         }
 
-    }
+        System.out.println("Hello " + name);
 
+    }
 }
