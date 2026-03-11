303b17a7c3d658cc8bbcd2751316a46e8eccd723
GOUTHU VEERA VENKATA SIVA SANTHOSH <santhoshgowthu@gmail.com>
2026-03-11 14:03:00 +0530

    UC4 - Display Hello with multiple names

---
+++ HelloMultipleNames.java
@@ -0,0 +1,17 @@
+public class HelloMultipleNames {
+
+    public static void main(String[] args) {
+
+        if(args.length == 0) {
+            System.out.println("Hello World");
+        }
+        else {
+
+            for(int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
+
+        }
+
+    }
+}
