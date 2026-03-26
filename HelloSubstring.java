82a82920468ddf2532911112e83ad83ac630f7db
GOUTHU VEERA VENKATA SIVA SANTHOSH <santhoshgowthu@gmail.com>
2026-03-26 13:28:52 +0530

    UC6 - Greeting using substring method

---
+++ HelloSubstring.java
@@ -0,0 +1,23 @@
+public class HelloSubstring {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } 
+        else {
+
+            String result = "";
+
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+            // remove last comma and space
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello " + result);
+        }
+
+    }
+}
