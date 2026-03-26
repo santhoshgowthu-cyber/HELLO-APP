3a8d52b7294d1ba8908791d9d019033d11de1a7f
GOUTHU VEERA VENKATA SIVA SANTHOSH <santhoshgowthu@gmail.com>
2026-03-26 13:33:01 +0530

    UC7 - Greeting using String.join()

---
+++ HelloJoin.java
@@ -0,0 +1,16 @@
+public class HelloJoin {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } 
+        else {
+
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names);
+
+        }
+
+    }
+}
